9fbcbc4dfd3029bb74ee7106427c250a54c794dd
JoeSechrist <joesechrist@gmail.com>
2017-02-21 18:35:46 -0500

    Removed 'otherwise' for url routing. Also updated title/description for index.html and details on package.json.

--- client/app/components/home/home.js
+++ client/app/components/home/home.js
@@ -10,8 +10,6 @@ let homeModule = angular.module('home', [
 .config(($stateProvider, $urlRouterProvider) => {
   "ngInject";
 
-  $urlRouterProvider.otherwise('/');
-
   $stateProvider
     .state('home', {
       url: '/info-loader/',
